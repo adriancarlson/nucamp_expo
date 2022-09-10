1130977fb03ede5af4c1bb06fac61af869068c19
Adrian Carlson <adrian.r.carlson@gmail.com>
2022-09-10 12:11:48 -0500

    Tab Navigation

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -18,6 +18,8 @@ import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import FavoritesScreen from './FavoritesScreen';
 import LoginScreen from './LoginScreen';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+
 const Drawer = createDrawerNavigator();
 
 const screenOptions = {
@@ -94,8 +96,11 @@ const LoginNavigator = () => {
 			<Stack.Screen
 				name='Login'
 				component={LoginScreen}
-				options={({ navigation }) => ({
-					headerLeft: () => <Icon name='sign-in' type='font-awesome' iconStyle={styles.stackIcon} onPress={() => navigation.toggleDrawer()}></Icon>,
+				options={({ navigation, route }) => ({
+					headerTitle: getFocusedRouteNameFromRoute(route),
+					headerLeft: () => (
+						<Icon name={getFocusedRouteNameFromRoute(route) === 'Register' ? 'user-plus' : 'sign-in'} type='font-awesome' iconStyle={styles.stackIcon} onPress={() => navigation.toggleDrawer()}></Icon>
+					),
 				})}
 			/>
 		</Stack.Navigator>
