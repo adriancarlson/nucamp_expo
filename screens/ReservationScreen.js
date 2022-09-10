3703dab9a8c15362606a85dca96d37659818baf3
Adrian Carlson <adrian.r.carlson@gmail.com>
2022-09-10 10:54:13 -0500

    Local Notifications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, StyleSheet, Switch, Button, Platform, Alert } f
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animateable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
 	const [campers, setCampers] = useState(1);
@@ -27,6 +28,33 @@ const ReservationScreen = () => {
 		setHikeIn(false);
 		setDate(new Date());
 		setShowCalendar(false);
+	};
+
+	const presentLocalNotification = async (reservationDate) => {
+		const sendNotifications = () => {
+			Notifications.setNotificationHandler({
+				handleNotification: async () => ({
+					shouldShowAlert: true,
+					shouldPlaySound: true,
+					shouldSetBadge: true,
+				}),
+			});
+			Notifications.scheduleNotificationAsync({
+				content: {
+					title: 'Your Campsite Reservation Search',
+					body: `Search for ${reservationDate} requested`,
+				},
+				trigger: null,
+			});
+		};
+
+		let permissions = await Notifications.getPermissionsAsync();
+		if (!permissions.granted) {
+			permissions = await Notifications.requestPermissionsAsync();
+		}
+		if (permissions.granted) {
+			sendNotifications();
+		}
 	};
 
 	return (
@@ -62,11 +90,17 @@ const ReservationScreen = () => {
 									{
 										text: 'Cancel',
 										style: 'cancel',
-										onPress: () => resetForm(),
+										onPress: () => {
+											console.log('Reservation Search Canceled');
+											resetForm();
+										},
 									},
 									{
 										text: 'OK',
-										onPress: () => resetForm(),
+										onPress: () => {
+											presentLocalNotification(date.toLocaleDateString('en-US'));
+											resetForm();
+										},
 									},
 								],
 								{ cancelable: false },
